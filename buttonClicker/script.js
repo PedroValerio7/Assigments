e5647ad44cb126131aff5968312b71e9295f4725
PedroValerio7 <88409097+PedroValerio7@users.noreply.github.com>
2021-08-21 18:00:58 -0500

    commit

--- buttonClicker/script.js
+++ buttonClicker/script.js
@@ -1,3 +1,15 @@
-function displayName(elementName) {
-    console.log(elementName)
+function login(element) {
+
+    if(element.innerText == "Login"){
+        element.innerText = "Logout";
+    }else{
+        element.innerText = "Login";
+    }
+
+    
+}
+
+function deleteD() {
+    var place = document.getElementById("definition");
+    place.remove();
 }
