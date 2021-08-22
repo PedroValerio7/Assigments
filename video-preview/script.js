31dc029c357ce880da1a1da5281e8280117c971f
PedroValerio7 <88409097+PedroValerio7@users.noreply.github.com>
2021-08-22 16:57:21 -0500

    commit

---
+++ video-preview/script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+function playV(element){
+    
+    document.getElementById("videoID").play();
+    
+}
+
+function pauseV(element){
+    
+    document.getElementById("videoID").pause();
+    
+}
