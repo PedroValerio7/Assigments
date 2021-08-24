f329561a8976103fbaf0a817ecca3c9c4b40e52d
PedroValerio7 <88409097+PedroValerio7@users.noreply.github.com>
2021-08-23 22:15:00 -0500

    commit

---
+++ likes/script.js
@@ -0,0 +1,10 @@
+
+function count(){
+    document.getElementById("num").value++;
+}
+function count2(){
+    document.getElementById("num2").value++;
+}
+function count3(){
+    document.getElementById("num3").value++;
+}
