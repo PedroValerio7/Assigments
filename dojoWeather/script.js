94156e1b59c4e5599a217a8f8d80ab299e3617c9
PedroValerio7 <88409097+PedroValerio7@users.noreply.github.com>
2021-08-24 21:27:43 -0500

    commit

---
+++ dojoWeather/script.js
@@ -0,0 +1,15 @@
+
+function hide() {
+    document.querySelector(".policy").remove();
+}
+
+function temp(element) {
+    document.querySelector("#red").innerHTML = "75°";
+    document.querySelector("#blue").innerHTML = "65°";
+    document.querySelector("#red2").innerHTML = "80°";
+    document.querySelector("#blue2").innerHTML = "66°";
+    document.querySelector("#red3").innerHTML = "69°";
+    document.querySelector("#blue3").innerHTML = "61°";
+    document.querySelector("#red4").innerHTML = "78°";
+    document.querySelector("#blue4").innerHTML = "70°";
+}
